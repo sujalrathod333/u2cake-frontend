efa2459e2eb5515e3fa130349137fd6128c08cf3
sujalrathod333 <sujalrathod223@gmail.com>
2026-06-03 16:03:14 +0530

    changes

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://u2-cake-backend.onrender.com/api",
   withCredentials: true,
 });
 
